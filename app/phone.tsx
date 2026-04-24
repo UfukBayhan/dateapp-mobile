ca6ecef840e037f3749f256c3402eac183ae2899
Ufuk Bayhan <0ufukbayhan6@gmail.com>
2026-04-24 16:41:44 +0300

    ios klavye düzenlenmesi

--- app/phone.tsx
+++ app/phone.tsx
@@ -111,6 +111,8 @@ export default function Phone({ onVerified }: { onVerified: (phone: string) => v
           onChangeText={setPhone}
           keyboardType="number-pad"
           maxLength={11}
+          returnKeyType="done"        // ← klavyede "Bitti" butonu çıkar
+          onSubmitEditing={handleSendCode}
         />
       </View>
 
